d431dad817720865da4ba0d93626e907bfd8f0df
Kamilla Faust <kamilla.cfaust@gmail.com>
2022-02-15 19:00:24 -0300

    end of project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import './App.css';
 import React, { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro';
-import {Container} from "@material-ui/core"
+import {Container, Typography } from "@material-ui/core"
+import '@fontsource/roboto';
 
 class App extends Component {
   
   render() {
   return (
     <Container component="article" maxWidth="sm">
-      <h1>Formulário de Cadastro</h1>
-      <FormularioCadastro />
+      <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
+      <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf = {validarCpf}/>
     </Container>
   );
 }
 }
+
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+
+function validarCpf(cpf) {
+  if (cpf.length !== 11) {
+    return {
+      valido: false, texto: "CPF deve conter 11 dígitos!"
+    };
+  } else {
+    return {
+      valido: true, texto: ""
+    };
+  }
+}
+
 
 export default App;
